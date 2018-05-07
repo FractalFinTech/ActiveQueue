c1cc14f36e0a94784e7087faf9defe1fb729c1d7
MagnusTiberius <bgonzales20001@gmail.com>
2018-05-07 10:42:35 -0500

    Initialize project repo

---
+++ src/main/java/fractalfintech/activequeue/ActivequeueApplication.java
@@ -0,0 +1,12 @@
+package fractalfintech.activequeue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivequeueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivequeueApplication.class, args);
+	}
+}
