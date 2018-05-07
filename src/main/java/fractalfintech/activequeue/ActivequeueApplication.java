1b572ae4b16762f03bf0b92dcb16487fc2061771
MagnusTiberius <bgonzales20001@gmail.com>
2018-05-07 11:51:12 -0500

    Add enable jms

--- src/main/java/fractalfintech/activequeue/ActivequeueApplication.java
+++ src/main/java/fractalfintech/activequeue/ActivequeueApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
@@ -15,6 +16,7 @@ import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
 @SpringBootApplication
+@EnableJms
 public class ActivequeueApplication {
 
     @Bean
